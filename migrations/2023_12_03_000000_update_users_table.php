bd48274a46e8f5412769829ad803e8be249986b2
other8026 <146984625+other8026@users.noreply.github.com>
2023-12-03 18:16:43 +0800

    fix column type
    originally using text, but not sure how to get `string` rather than `text` using the old way, so using this other way based on the add nickname extension.

--- migrations/2023_12_03_000000_update_users_table.php
+++ migrations/2023_12_03_000000_update_users_table.php
@@ -1,7 +1,23 @@
 <?php
 
-use Flarum\Database\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
 
-return Migration::addColumns('users', [
-    'flair' => ['string', 'nullable' => true],
-]);
+/*
+ * based on https://github.com/flarum/nicknames/blob/main/migrations/2020_11_23_000000_add_nickname_column.php
+ */
+
+return [
+    'up' => function (Builder $schema) {
+        if (! $schema->hasColumn('users', 'role-flair')) {
+            $schema->table('users', function (Blueprint $table) {
+                $table->string('role-flair', 100)->index()->nullable();
+            });
+        }
+    },
+    'down' => function (Builder $schema) {
+        $schema->table('users', function (Blueprint $table) {
+            $table->dropColumn('role-flair');
+        });
+    }
+];
