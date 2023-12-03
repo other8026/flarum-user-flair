b6d6dcf14469fb4c19a223a46f11d03c86d4d696
other8026 <146984625+other8026@users.noreply.github.com>
2023-12-03 23:30:00 +0800

    move strings to yml file

--- src/FlairValidator.php
+++ src/FlairValidator.php
@@ -8,6 +8,7 @@ namespace Other8026\UserFlair;
 
 use Flarum\Settings\SettingsRepositoryInterface;
 use Illuminate\Validation\Validator;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class FlairValidator
 {
@@ -16,9 +17,15 @@ class FlairValidator
      */
     protected $settings;
 
-    public function __construct(SettingsRepositoryInterface $settings)
+    /**
+     * @var TranslatorInterface
+     */
+    protected $translator;
+
+    public function __construct(SettingsRepositoryInterface $settings, TranslatorInterface $translator)
     {
         $this->settings = $settings;
+        $this->translator = $translator;
     }
 
     public function __invoke($flarumValidator, Validator $validator)
@@ -29,7 +36,7 @@ class FlairValidator
             function ($attribute, $value, $fail) {
                 $regex = $this->settings->get('user_flair.validator.regex');
                 if ($regex && ! preg_match_all("/$regex/", $value)) {
-                    $fail("username is not valid");
+                    $fail($this->translator->trans('other8026-user-flair.api.invalid'));
                 }
             },
             'min:'.$this->settings->get('user_flair.validator.min-length'),
