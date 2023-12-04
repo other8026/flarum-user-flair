c3941efca21e8abf20637a42db450a2f96464448
other8026 <146984625+other8026@users.noreply.github.com>
2023-12-05 01:40:09 +0800

    Update UserPolicy.php

--- src/Access/UserPolicy.php
+++ src/Access/UserPolicy.php
@@ -19,8 +19,15 @@ class UserPolicy extends AbstractPolicy
      */
     public function editFlair(User $actor, User $user)
     {
+        // do in this order:
+        // non-admins can't edit admins' flair
+        // admins can always make changes
+        // permit changes if the actor has privileges to edit users (a different permission)
+        // deny if none of the preceding conditions are met
         if (!$actor->isAdmin() && $user->isAdmin()) {
             return $this->deny();
+        } else if ($actor->isAdmin()) {
+            return $this->allow();
         } else if ($actor->can('edit', $user)) {
             return $this->allow();
         } else {
