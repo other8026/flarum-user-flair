6bcf18d167171e760465fbaa02129dda340a7993
other8026 <146984625+other8026@users.noreply.github.com>
2023-12-05 00:10:19 +0800

    flair falls back to set default

--- src/AddUserFlairAttributes.php
+++ src/AddUserFlairAttributes.php
@@ -7,18 +7,33 @@
 namespace Other8026\UserFlair;
 
 use Flarum\Api\Serializer\UserSerializer;
+use Flarum\Settings\SettingsRepositoryInterface;
 use Flarum\User\User;
 
 class AddUserFlairAttributes
 {
+    /**
+     * @var SettingsRepositoryInterface
+     */
+    protected $settings;
+
+    public function __construct(SettingsRepositoryInterface $settings)
+    {
+        $this->settings = $settings;
+    }
     public function __invoke(UserSerializer $serializer, User $user): array
     {
         $attributes = [];
 
+        // add user's flair if set
+        // add null if no user flair set and no default
+        // add default flair if it's set and no user flair is set
         if ($user->user_flair && strlen($user->user_flair) > 0) {
             $flair = $user->user_flair;
+        } else if ($this->settings->get('user_flair.default_flair') && strlen($this->settings->get('user_flair.default_flair')) > 0) {
+            $flair = $this->settings->get('user_flair.default_flair');
         } else {
-            $flair = "temporary thing";
+            $flair = null;
         }
 
         $attributes['userFlair'] = $flair;
