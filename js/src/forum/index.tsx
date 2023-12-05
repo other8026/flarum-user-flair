31e1635fec76349139a1a021c9322777b1292e56
other8026 <146984625+other8026@users.noreply.github.com>
2023-12-06 00:33:32 +0800

    override `PostUser`'s `view()` method

--- js/src/forum/index.tsx
+++ js/src/forum/index.tsx
@@ -1,9 +1,15 @@
 import {extend} from 'flarum/common/extend';
+import {override} from 'flarum/common/extend';
+import Link from 'flarum/common/components/Link';
+import UserCard from 'flarum/forum/components/UserCard';
 import app from 'flarum/forum/app';
+import avatar from 'flarum/common/helpers/avatar';
+import username from 'flarum/common/helpers/username';
+import userOnline from 'flarum/common/helpers/userOnline';
+import listItems from 'flarum/common/helpers/listItems';
 import UserControls from 'flarum/forum/utils/UserControls'
 import Button from 'flarum/common/components/Button';
-// import Badge from 'flarum/common/components/Badge';
-// import User from 'flarum/common/models/User';
+import PostUser from 'flarum/forum/components/PostUser';
 
 export {default as extend} from './extendUser';
 
@@ -18,5 +24,44 @@ app.initializers.add('other8026/user-flair', () => {
         </Button>
       );
     }
+  });
+
+  override(PostUser.prototype, 'view', function () {
+    // flarum doesn't make it easy to modify this part of forum,
+    // so this needs to be copied and pasted from the source,
+    // just need to add the small bit of stuff for the user's flair
+    // https://github.com/flarum/flarum-core/blob/v1.8.0/js/src/forum/components/PostUser.js
+
+    const post = this.attrs.post;
+    const user = post.user();
+
+    if (!user) {
+      return (
+        <div className="PostUser">
+          <h3 className="PostUser-name">
+            {avatar(user, {className: 'PostUser-avatar'})} {username(user)}
+          </h3>
+        </div>
+      );
+    }
+
+    return (
+      <div className="PostUser">
+        <h3 className="PostUser-name">
+          <Link href={app.route.user(user)}>
+            {avatar(user, {className: 'PostUser-avatar'})}
+            {userOnline(user)}
+            {username(user)}
+            <span class="user-flair">{user.userFlair()}</span>
+          </Link>
+        </h3>
+        <ul className="PostUser-badges badges">{listItems(user.badges().toArray())}</ul>
+
+        {!post.isHidden() && this.attrs.cardVisible && (
+          <UserCard user={user} className="UserCard--popover"
+                    controlsButtonClassName="Button Button--icon Button--flat"/>
+        )}
+      </div>
+    );
   });
 });
